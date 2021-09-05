ae4d63fbaf1770fe0e771863ce6bec2cc1e94c41
Tracer1337 <merlin.moelter@gmail.com>
2021-09-05 14:24:25 +0200

    Integrate common package into server

--- packages/server/src/Server.java.ts
+++ packages/server/src/Server.java.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express"
 import cors from "cors"
 import ScoreCalculator from "github/dist/ScoreCalculator.java"
+import Utils from "common/dist/Utils.java"
 
 export default class Server {
     private static readonly PORT = process.env.PORT
@@ -25,18 +26,20 @@ export default class Server {
     }
 
     private async handleGithubScore(req: Request, res: Response) {
-        try {
-            const username = req.params.username
-            const scoreCalculator = new ScoreCalculator(
-                username,
-                process.env.PERSONAL_ACCESS_TOKEN
-            )
-            const score = await scoreCalculator.getScore()
-            res.send({
-                data: score
-            })
-        } catch {
-            res.sendStatus(500)
+        const username = req.params.username
+        const scoreCalculator = new ScoreCalculator(
+            username,
+            process.env.PERSONAL_ACCESS_TOKEN
+        )
+        const [error, score] = await Utils.promise(
+            scoreCalculator.getScore()
+        )
+        if (error) {
+            console.error(error)
+            return void res.sendStatus(500)
         }
+        res.send({
+            data: score
+        })
     }
 }
