7e5b0c4f3a161d8305ea180ce4c0a828b087663b
Tracer1337 <merlin.moelter@gmail.com>
2021-09-06 18:24:14 +0200

    Trigger pages workflow

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@material-ui/core"
 export default function Header() {
     return (
         <Typography variant="h3">
-            How many camels is your GitHub account worth?
+            Test
         </Typography>
     )
 }
