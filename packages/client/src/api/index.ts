7c5b4e67400271ca33b6c44717b1a1e631f93c4a
Tracer1337 <merlin.moelter@gmail.com>
2021-09-06 18:10:51 +0200

    Add pages workflow

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -1,17 +1,17 @@
 import API from "./types"
 
 function url(path: string) {
-    return `http://localhost:8080${path}`
+    return `${process.env.REACT_APP_API_ENDPOINT}${path}`
 }
 
 export async function fetchUser(username: string): Promise<API.User> {
-    const res = await fetch(url(`/discovery/github/${username}`))
+    const res = await fetch(url(`discovery/github/${username}`))
     const json = await res.json()
     return json.data
 }
 
 export async function fetchScore(username: string) {
-    const res = await fetch(url(`/score/github/${username}`))
+    const res = await fetch(url(`score/github/${username}`))
     const json = await res.json()
     return json.data
 }
